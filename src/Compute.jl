c3da3c593b60e6335338e3ace4c467d141c5d3d3
Jeffrey Varner <jvarner@paliquant.com>
2022-02-27 19:06:58 -0500

    Update Compute.jl
    working on expiration

--- src/Compute.jl
+++ src/Compute.jl
@@ -1,16 +1,51 @@
 # == PRIVATE METHODS BELOW HERE ================================================================================================= #
-function _payoff(contract::PutContractModel, underlying::Float64)::Float64 end
+function _expiration(contract::PutContractModel, underlying::Float64)::Tuple{Float64,Float64}
 
-function _payoff(contract::CallContractModel, underlying::Float64)::Float64
-    return 0.0
+    # get data from the contract model - 
+    direction = contract.direction
+    K = contract.strike_price
+    premium = contract.premium
+    payoff_value = 0.0
+    profit_value = 0.0
+
+    # PUT contract -
+    payoff_value = direction * max((K - underlying), 0.0)
+    profit_value = payoff_value - direction * premium
+
+    # return -
+    return (payoff_value, profit_value)
 end
 
-function _payoff(equity::EquityModel, underlying::Float64)::Float64
-    return 0.0
+function _expiration(contract::CallContractModel, underlying::Float64)::Tuple{Float64,Float64}
+
+    # get data from the contract model - 
+    direction = contract.direction
+    K = contract.strike_price
+    premium = contract.premium
+    payoff_value = 0.0
+    profit_value = 0.0
+
+    # PUT contract -
+    payoff_value = direction * max((underlying - K), 0.0)
+    profit_value = payoff_value - direction * premium
+
+    # return -
+    return (payoff_value, profit_value)
 end
 
+function _expiration(equity::EquityModel, underlying::Float64)::Tuple{Float64,Float64}
 
+    # get data from Equity model -
+    direction = contract.direction
+    purchase_price = contract.purchase_price
 
+    # Equity -
+    payoff_value = (underlying - purchase_price)
+    profit_value = payoff_value
+
+    # return -
+    return (payoff_value, profit_value)
+end
 # == PRIVATE METHODS ABOVE HERE ================================================================================================= #
 
 # == PUBLIC METHODS BELOW HERE ================================================================================================== #
@@ -27,12 +62,13 @@ function expiration(model::T, underlying::Array{Float64,1})::DataFrame where {T<
     for (i, S) ∈ enumerate(underlying)
 
         # compute the payoff -
-        payoff_value = _payoff(model, S)
+        (payoff_value, profit_value) = _expiration(model, S)
 
         # compute the payoff -
         data_tuple = (
             underlying = S,
-            payoff = payoff_value
+            payoff = payoff_value,
+            profit = profit_value
         )
 
         # push -
@@ -41,5 +77,9 @@ function expiration(model::T, underlying::Array{Float64,1})::DataFrame where {T<
 
     # return -
     return expiration_data_table
+end
+
+function expiration(models::Array{T,1}, underlying::Array{Float64,1})::DataFrame where {T<:AbstractAssetModel}
+    return DataFrame()
 end
 # == PUBLIC METHODS ABIVE HERE ================================================================================================== #
