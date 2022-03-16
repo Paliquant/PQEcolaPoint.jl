b02a24e0e1c7e04e044365d8408f87b6ee9e6c96
Jeffrey Varner <jvarner@paliquant.com>
2022-03-16 19:11:57 -0400

    Update test_price_level_filter.jl
    nice

--- test/test_price_level_filter.jl
+++ test/test_price_level_filter.jl
@@ -1,7 +1,7 @@
 using PQEcolaPoint
 
 # build a binary tree with three levels -
-model = build(CRRLatticeModel; number_of_levels = 4, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
+model = build(CRRLatticeModel; number_of_levels = 12, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
 
 # compute -
-tmp_array = price(model, 4)
+tmp_array = price(model, 12)
