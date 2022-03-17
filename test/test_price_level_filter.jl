e2e90563c8c23a93b42ca5ed8b7ae6337c361544
Jeffrey Varner <jvarner@paliquant.com>
2022-03-17 06:52:51 -0400

    added stats base
    added stats base

--- test/test_price_level_filter.jl
+++ test/test_price_level_filter.jl
@@ -1,7 +1,7 @@
 using PQEcolaPoint
 
 # build a binary tree with three levels -
-model = build(CRRLatticeModel; number_of_levels = 12, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
+model = build(CRRLatticeModel; number_of_levels = 3, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
 
 # compute -
-tmp_array = price(model, 12)
+tmp_array = price(model, 3)
