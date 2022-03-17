b44bbda08b7a0994a563f2ac396cd32f4a657fd9
Jeffrey Varner <jvarner@paliquant.com>
2022-03-17 09:20:55 -0400

    Update test_price_level_filter.jl
    need to compute p coming out

--- test/test_price_level_filter.jl
+++ test/test_price_level_filter.jl
@@ -1,7 +1,7 @@
 using PQEcolaPoint
 
 # build a binary tree with three levels -
-model = build(CRRLatticeModel; number_of_levels = 3, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
+model = build(CRRLatticeModel; number_of_levels = 4, σ = 0.35, T = (14.0 / 365), μ = 0.00174)
 
 # compute -
-tmp_array = price(model, 3)
+tmp_array = price(model, 4)
