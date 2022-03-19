ca3eb9f2ec7e079157ad88705a72faf76f660217
Jeffrey Varner <jvarner@paliquant.com>
2022-03-19 19:37:54 -0400

    hmmm
    delta

--- src/Greeks.jl
+++ src/Greeks.jl
@@ -1,10 +1,10 @@
-function θ(contract::Y; number_of_levels::Int64 = 2, T::Float64 = (1 / 365), σ::Float64 = 0.15, 
-    Sₒ::Float64 = 1.0, μ::Float64 = 0.0015, 
+function θ(contract::Y; number_of_levels::Int64=2, T::Float64=(1 / 365), σ::Float64=0.15,
+    Sₒ::Float64=1.0, μ::Float64=0.0015,
     choice::Function=_rational)::Float64 where {Y<:AbstractDerivativeContractModel}
 
     # θ : 1 day diff 
     Tₒ = T
-    T₁ = Tₒ - (1/365)
+    T₁ = Tₒ - (1 / 365)
 
     # build a binary tree with N levels -
     mₒ = build(CRRLatticeModel; Sₒ=Sₒ, number_of_levels=number_of_levels, σ=σ, T=Tₒ, μ=μ)
@@ -21,15 +21,29 @@ function θ(contract::Y; number_of_levels::Int64 = 2, T::Float64 = (1 / 365), σ
     return θ_value
 end
 
-function δ(contract::Y; number_of_levels::Int64 = 2, T::Float64 = (1 / 365), σ::Float64 = 0.15, 
-    Sₒ::Float64 = 1.0, μ::Float64 = 0.0015, 
-    choice::Function=_rational)
+function δ(contract::Y; number_of_levels::Int64=2, T::Float64=(1 / 365), σ::Float64=0.15,
+    Sₒ::Float64=1.0, μ::Float64=0.0015,
+    choice::Function=_rational) where {Y<:AbstractDerivativeContractModel}
 
-    
+    # advance base price by 1 -
+    S₁ = Sₒ + 1
 
+    # build models -
+    mₒ = build(CRRLatticeModel; Sₒ=Sₒ, number_of_levels=number_of_levels, σ=σ, T=T, μ=μ)
+    m₁ = build(CRRLatticeModel; Sₒ=S₁, number_of_levels=number_of_levels, σ=σ, T=T, μ=μ)
+
+    # compute -
+    Pₒ = premium(contract, mₒ; choice=choice)
+    P₁ = premium(contract, m₁; choice=choice)
+
+    # compute theta -
+    δ_value = (P₁ - Pₒ)
+
+    # return the value -
+    return δ_value
 end
 
-function δ(contract::Y, mₒ::CRRLatticeModel, m₁::CRRLatticeModel; 
+function δ(contract::Y, mₒ::CRRLatticeModel, m₁::CRRLatticeModel;
     choice::Function=_rational)::Float64 where {Y<:AbstractDerivativeContractModel}
 
     # compute -
