3510533483e492d67131acfeed0faa54402715b8
Jeffrey Varner <jvarner@paliquant.com>
2022-04-25 16:51:22 -0400

    hmmm
    gamma is not working

--- test/test_compute_gamma.jl
+++ test/test_compute_gamma.jl
@@ -5,10 +5,11 @@ using Dates
 short_put_contract = PutContractModel()
 short_put_contract.ticker = "XYZ"
 short_put_contract.expiration_date = Date(2022, 04, 14)
-short_put_contract.strike_price = 105.0
+short_put_contract.strike_price = 65.0
 short_put_contract.premium = 0.0
 short_put_contract.number_of_contracts = 1
 short_put_contract.direction = 1
 
 # other version -
-val_γ = γ(short_put_contract; Sₒ=113.46, number_of_levels=80, σ=0.5212, T=(27.0 / 365), μ=0.0047)
+# origin_point = encode(JIT_lattice; S = 70.12, DTE = 53.0, IV = 0.4926, K = 65.0)
+val_γ = γ(short_put_contract; Sₒ=70.12, number_of_levels=80, σ=0.5129, T=(53.0 / 365), μ=0.0047)
--- test/test_roll.jl
+++ test/test_roll.jl
@@ -23,7 +23,7 @@ K = range(45.0, stop = 90.0, step = 5.0) |> collect
 JIT_lattice = build(CRRJITContractPremiumLatticeModel, PutContractModel, S, K, IV_array, DTE; number_of_levels = 80)
 
 # origin version -
-origin_point = encode(JIT_lattice; S = 70.12, DTE = 53.0, IV = 0.4926, K = 65.0)
+origin_point = encode(JIT_lattice; S = 70.12, DTE = 53.0, IV = 0.5129, K = 65.0)
 start_point = encode(JIT_lattice; S = 68.41, DTE = 56.0, IV = 0.5957, K = 65.0)
 end_point = encode(JIT_lattice; S = 68.41 , DTE = 84.0, IV = 0.5737, K = 55.0)
 C = roll(JIT_lattice, origin_point, start_point=>end_point)
