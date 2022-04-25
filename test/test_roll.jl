40857ed180dce288804d9271a3af33797ed45e82
Jeffrey Varner <jvarner@paliquant.com>
2022-04-25 15:33:16 -0400

    Update test_roll.jl
    working on roll

--- test/test_roll.jl
+++ test/test_roll.jl
@@ -26,6 +26,6 @@ test = roll(JIT_lattice, start_point=>end_point)
 
 # origin version -
 origin_point = encode(JIT_lattice; S = 70.98, DTE = 56.0, IV = 0.53, K = 65.0)
-start_point = encode(JIT_lattice; S = 69.41, DTE = 56.0, IV = 0.5957, K = 65.0)
-end_point = encode(JIT_lattice; S = 69.41 , DTE = 63.0, IV = 0.5818, K = 55.0)
-test_2 = roll(JIT_lattice, origin_point, start_point=>end_point)
+start_point = encode(JIT_lattice; S = 68.41, DTE = 56.0, IV = 0.5957, K = 65.0)
+end_point = encode(JIT_lattice; S = 68.41 , DTE = 84.0, IV = 0.5737, K = 55.0)
+C = roll(JIT_lattice, origin_point, start_point=>end_point)
